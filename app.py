3fe00ff3a81f465aca7d0b0fbca9adc6226003b4
Ziqian Gao <ziqiangao@users.noreply.huggingface.co>
2024-07-16 20:17:33 +0000

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ import ffmpeg
 import subprocess
 import traceback
 
-path = '/'  # Update with your path
+path = os.getcwd()  # Update with your path
 
 def getRenderCords(ta: list, idx: int, res: int = 1024, size: tuple = (1280, 720)) -> list:
     i = idx - res // 2
