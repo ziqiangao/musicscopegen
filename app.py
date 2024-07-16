0f4305ec19c36e0fa20479a46935d3976a84ef67
ziqiangao <88281267+ziqiangao@users.noreply.github.com>
2024-07-16 20:38:00 +0100

    fix error and add fonts

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ import ffmpeg
 import subprocess
 import traceback
 
-path = '/app/'  # Update with your path
+path = 'C:/Users/ziqia/Downloads/oooo/'  # Update with your path
 
 def getRenderCords(ta: list, idx: int, res: int = 1024, size: tuple = (1280, 720)) -> list:
     i = idx - res // 2
